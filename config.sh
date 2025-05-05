d427acaef0fc0f2e75f9e15a3bb4be74706650ab
François Sémécurbe <francoissemecurbe@gmail.com>
2025-05-05 20:07:28 +0000

    Update config.sh

--- config.sh
+++ config.sh
@@ -5,3 +5,8 @@ sudo apt -y install filezilla
 
 sudo locale-gen oc_FR.UTF-8
 sudo update-locale
+
+sudo cp /usr/share/X11/xkb/symbols/fr /usr/share/X11/xkb/symbols/oc
+sudo nano /usr/share/X11/xkb/symbols/oc
+sudo nano /usr/share/X11/xkb/rules/evdev.xml
+sudo dpkg-reconfigure xkb-data
